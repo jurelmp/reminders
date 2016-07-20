a50c9ae084665ab01feedf5e7f8467774f68c52e
JurelP <patocjurel@gmail.com>
2016-07-20 14:47:17 +0800

    Adds SQLite database persistence for reminders and a new color for important reminders

--- app/src/main/java/com/jurelmp/reminders/RemindersActivity.java
+++ app/src/main/java/com/jurelmp/reminders/RemindersActivity.java
@@ -1,17 +1,21 @@
 package com.jurelmp.reminders;
 
-import android.support.v7.app.AppCompatActivity;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import java.sql.SQLException;
 
 public class RemindersActivity extends AppCompatActivity {
 
     private ListView mListView;
+    private RemindersDbAdapter mDbAdapter;
+    private RemindersSimpleCursorAdapter mCursorAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,18 +23,40 @@ public class RemindersActivity extends AppCompatActivity {
         setContentView(R.layout.activity_reminders);
 
         mListView = (ListView) findViewById(R.id.reminders_list_view);
+        mListView.setDivider(null);
+        mDbAdapter = new RemindersDbAdapter(this);
+        try {
+            mDbAdapter.open();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
-        /**
-         * The array adapter is the controller in our
-         * model-view-controller relationship. (controller).
-         */
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
-                this,
+        Cursor cursor = mDbAdapter.fetchAllReminders();
+
+        // from columns defined in the db
+        String[] from = new String[]{RemindersDbAdapter.COL_CONTENT};
+
+        // to the ids of views in the layout
+        int[] to = new int[]{R.id.row_text};
+
+        mCursorAdapter = new RemindersSimpleCursorAdapter(
+                // context
+                RemindersActivity.this,
+                // the layout of the row
                 R.layout.reminders_row,
-                R.id.row_text,
-                new String[]{"first record", "second record", "third record"});
+                // cursor
+                cursor,
+                // from columns defined in the db
+                from,
+                // to the ids of views in the layout
+                to,
+                // flag = not used
+                0
+        );
 
-        mListView.setAdapter(arrayAdapter);
+        // the cursorAdapter (controller) is now updating the listView (view)
+        // with the data from the db (model)
+        mListView.setAdapter(mCursorAdapter);
     }
 
     @Override
---
+++ app/src/main/java/com/jurelmp/reminders/RemindersDbAdapter.java
@@ -0,0 +1,147 @@
+package com.jurelmp.reminders;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import java.sql.SQLException;
+
+/**
+ * Created by Jurel on 7/20/2016.
+ */
+public class RemindersDbAdapter {
+
+    // These are the column names
+    public static final String COL_ID = "_id";
+    public static final String COL_CONTENT = "content";
+    public static final String COL_IMPORTANT = "important";
+
+    // These are the corresponding indices
+    public static final int INDEX_ID = 0;
+    public static final int INDEX_CONTENT = INDEX_ID + 1;
+    public static final int INDEX_IMPORTANT = INDEX_ID + 2;
+
+    // Used for logging
+    private static final String TAG = "RemindersDbAdapter";
+    private static final String DATABASE_NAME = "dba_remdrs";
+    private static final String TABLE_NAME = "tbl_remdrs";
+    private static final int DATABASE_VERSION = 1;
+    // SQL statement used to create the database
+    private static final String DATABASE_CREATE =
+            "CREATE TABLE IF NOT EXISTS " + TABLE_NAME + " ( " +
+                    COL_ID + "INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    COL_CONTENT + " TEXT, " +
+                    COL_IMPORTANT + " INTEGER );";
+    private final Context mCtx;
+    private DatabaseHelper mDbHelper;
+    private SQLiteDatabase mDb;
+
+    public RemindersDbAdapter(Context mCtx) {
+        this.mCtx = mCtx;
+    }
+
+    // open
+    public void open() throws SQLException {
+        mDbHelper = new DatabaseHelper(mCtx);
+        mDb = mDbHelper.getWritableDatabase();
+    }
+
+    // close
+    public void close() {
+        if (mDbHelper != null) {
+            mDbHelper.close();
+        }
+    }
+
+    // CREATE
+    // note that the id will be created for you automatically
+    public void createReminder(String name, boolean important) {
+        ContentValues values = new ContentValues();
+        values.put(COL_CONTENT, name);
+        values.put(COL_IMPORTANT, important ? 1 : 0);
+        mDb.insert(TABLE_NAME, null, values);
+    }
+
+    // overloaded to take a reminder
+    public long createReminder(Reminder reminder) {
+        ContentValues values = new ContentValues();
+        values.put(COL_CONTENT, reminder.getmContent());
+        values.put(COL_IMPORTANT, reminder.getmImportant());
+
+        // Inserting row
+        return mDb.insert(TABLE_NAME, null, values);
+    }
+
+    // READ
+    public Reminder fetchReminderById(int id) {
+        Cursor cursor = mDb.query(
+                TABLE_NAME,
+                new String[]{COL_ID, COL_CONTENT, COL_IMPORTANT},
+                COL_ID + "=?", new String[]{String.valueOf(id)}, null, null, null, null);
+
+        if (cursor != null)
+            cursor.moveToFirst();
+
+        return new Reminder(
+                cursor.getInt(INDEX_ID),
+                cursor.getString(INDEX_CONTENT),
+                cursor.getInt(INDEX_IMPORTANT)
+        );
+    }
+
+    // FETCH ALL
+    public Cursor fetchAllReminders() {
+        Cursor mCursor = mDb.query(
+                TABLE_NAME,
+                new String[]{COL_ID, COL_CONTENT, COL_IMPORTANT},
+                null, null, null, null, null);
+
+        if (mCursor != null) {
+            mCursor.moveToFirst();
+        }
+        return mCursor;
+    }
+
+    // UPDATE
+    public void updateReminder(Reminder reminder) {
+        ContentValues values = new ContentValues();
+        values.put(COL_CONTENT, reminder.getmContent());
+        values.put(COL_IMPORTANT, reminder.getmImportant());
+        mDb.update(TABLE_NAME, values,
+                COL_ID + "=?", new String[]{String.valueOf(reminder.getmId())});
+    }
+
+    // DELETE
+    public void deleteReminderById(int nId) {
+        mDb.delete(TABLE_NAME, COL_ID + "=?", new String[]{String.valueOf(nId)});
+    }
+
+    public void deleteAllReminders() {
+        mDb.delete(TABLE_NAME, null, null);
+    }
+
+    // SQLite database helper
+    private static class DatabaseHelper extends SQLiteOpenHelper {
+        public DatabaseHelper(Context context) {
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            Log.w(TAG, DATABASE_CREATE);
+            db.execSQL(DATABASE_CREATE);
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            Log.w(TAG, "Upgrading database from version " + oldVersion + " to " + newVersion + ", which will destroy all old data");
+            db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+            onCreate(db);
+        }
+    }
+}
+
+
