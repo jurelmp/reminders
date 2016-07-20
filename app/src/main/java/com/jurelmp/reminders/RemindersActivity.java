d53665eecdab66f3c77b25cb2727508c1e02d601
JurelP <patocjurel@gmail.com>
2016-07-20 13:48:56 +0800

    Adds new reminder and exit menu options

--- app/src/main/java/com/jurelmp/reminders/RemindersActivity.java
+++ app/src/main/java/com/jurelmp/reminders/RemindersActivity.java
@@ -2,6 +2,10 @@ package com.jurelmp.reminders;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -27,5 +31,29 @@ public class RemindersActivity extends AppCompatActivity {
                 new String[]{"first record", "second record", "third record"});
 
         mListView.setAdapter(arrayAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_reminders, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_new:
+                /**
+                 * Create new Reminder
+                 */
+                Log.d(getLocalClassName(), "create new Reminder");
+                return true;
+            case R.id.action_exit:
+                finish();
+                return true;
+            default:
+                return false;
+        }
     }
 }
