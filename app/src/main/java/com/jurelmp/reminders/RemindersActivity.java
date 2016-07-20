27ede2a01088bc3e3667bbd296b6a70981dc6455
JurelP <patocjurel@gmail.com>
2016-07-20 13:28:20 +0800

    Initial commit

---
+++ app/src/main/java/com/jurelmp/reminders/RemindersActivity.java
@@ -0,0 +1,31 @@
+package com.jurelmp.reminders;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class RemindersActivity extends AppCompatActivity {
+
+    private ListView mListView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reminders);
+
+        mListView = (ListView) findViewById(R.id.reminders_list_view);
+
+        /**
+         * The array adapter is the controller in our
+         * model-view-controller relationship. (controller)
+         */
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                this,
+                R.layout.reminders_row,
+                R.id.row_text,
+                new String[]{"first record", "second record", "third record"});
+
+        mListView.setAdapter(arrayAdapter);
+    }
+}
