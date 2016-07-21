5c77879b8d6373057bb2a8a1c49ad6ca10f96f3e
JurelP <patocjurel@gmail.com>
2016-07-21 14:59:49 +0800

    Fixes deprecation warnings

--- app/src/main/java/com/jurelmp/reminders/RemindersActivity.java
+++ app/src/main/java/com/jurelmp/reminders/RemindersActivity.java
@@ -31,7 +31,7 @@ import android.widget.TextView;
 import android.widget.TimePicker;
 
 import java.sql.SQLException;
-import java.util.Calendar;
+import java.util.Date;
 
 public class RemindersActivity extends AppCompatActivity {
 
@@ -123,17 +123,15 @@ public class RemindersActivity extends AppCompatActivity {
                             mDbAdapter.deleteReminderById(getIdFromPosition(masterListPosition));
                             mCursorAdapter.changeCursor(mDbAdapter.fetchAllReminders());
                         } else {
+                            final Date today = new Date();
                             TimePickerDialog.OnTimeSetListener listener = new TimePickerDialog.OnTimeSetListener() {
                                 @Override
                                 public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-                                    final Calendar alarmTime = Calendar.getInstance();
-                                    alarmTime.set(Calendar.HOUR, hourOfDay);
-                                    alarmTime.set(Calendar.MINUTE, minute);
-                                    scheduleReminder(alarmTime.getTimeInMillis(), reminder.getmContent());
+                                    Date alarm = new Date(today.getYear(), today.getMonth(), today.getDate(), hourOfDay, minute);
+                                    scheduleReminder(alarm.getTime(), reminder.getmContent());
                                 }
                             };
-                            final Calendar today = Calendar.getInstance();
-                            new TimePickerDialog(RemindersActivity.this, null, today.get(Calendar.HOUR), today.get(Calendar.MINUTE), false).show();
+                            new TimePickerDialog(RemindersActivity.this, null, today.getHours(), today.getMinutes(), false).show();
                         }
                         dialog.dismiss();
                     }
